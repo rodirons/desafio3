18d445e2c889ce4e6af58754c457d28da253b8c6
rodirons <rodirons86@gmail.com>
2023-11-25 14:10:57 +0000

    primeiro

---
+++ index.js
@@ -0,0 +1,29 @@
+class Hero {
+    constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo;
+    }
+  
+    atacar() {
+      let ataque = "";
+      switch (this.tipo) {
+        case "mago":
+          ataque = "magia";
+          break;
+        case "guerreiro":
+          ataque = "espada";
+          break;
+        case "monge":
+          ataque = "artes marciais";
+          break;
+        case "ninja":
+          ataque = "shuriken";
+          break;
+      }
+      console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+  }
+  
+  const meuGuerreiro = new Hero("Rodnelson", 37, "ninja");
+  meuGuerreiro.atacar();
